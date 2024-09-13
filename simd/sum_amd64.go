915b7eadb3e771863142eea1730dbebb46ba45c1
alex.zakarov <semerci394@gmail.com>
2024-09-13 21:36:19 +0300

    feat(root): base app

---
+++ simd/sum_amd64.go
@@ -0,0 +1,14 @@
+//go:build !noasm
+// +build !noasm
+
+package simd
+
+func init() {
+	if cpu.X86.HasAVX2 {
+		sumFloat64 = sum_float64_avx2
+	} else if cpu.X86.HasSSE42 {
+		sumFloat64 = sum_float64_sse4
+	} else {
+		sumFloat64 = sum_float64_go
+	}
+}
---
+++ simd/sum_noasm.go
@@ -0,0 +1,8 @@
+//go:build noasm
+// +build noasm
+
+package simd
+
+func init() {
+	sumFloat64 = sum_float64_go
+}
